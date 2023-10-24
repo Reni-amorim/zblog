ef56b36c07deb51b07aa0c27374979de8a1b98aa
Reni-amorim <reni.425@gmail.com>
2023-10-24 06:33:42 +0000

    set unix path in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix:///home/deploy/apps/zblog/shared/tmp/sockets/zblog-puma.sock;"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
@@ -113,7 +113,7 @@ namespace :deploy do
     end
   end
 
-  
+
   desc 'Restart application'
     task :restart do
       on roles(:app), in: :sequence, wait: 5 do
