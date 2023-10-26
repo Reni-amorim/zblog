32889ce572183a116369fd2e22db7165da3d86a2
Reni-amorim <reni.425@gmail.com>
2023-10-26 14:51:08 +0000

    set server ip and roles in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  server '15.229.43.153', user: 'deploy', roles: %w{app db web}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
